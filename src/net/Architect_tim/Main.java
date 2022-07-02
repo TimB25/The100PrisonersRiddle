a085f0839b1d01cef5e206d032b3ba8f7c3fec61
BuildTools <unconfigured@null.spigotmc.org>
2022-07-02 19:12:53 +0200

    setup completed

--- src/net/Architect_tim/Main.java
+++ src/net/Architect_tim/Main.java
@@ -1,5 +1,6 @@
 package net.Architect_tim;
 
+import java.util.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -10,36 +11,73 @@ public class Main {
 
     public static void main(String[] args) {
         int prisioners = 100;
+        int aantalPogingen = 50;
 
-
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter number of prisioners:");
 
         try {
             prisioners = sc.nextInt();
-        } catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("ERROR: Enter a number! Rerun the program or the number of prisioners wil be the default of 100!");
         }
-        System.out.println(prisioners);
+        System.out.println("you have chosen that there wil be " + prisioners+" prisioners.");
+        try {
+            aantalPogingen = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("ERROR: Enter a number! Rerun the program or the number of aantalPogingen wil be the default of 50!");
+        }
+        System.out.println("you have chosen that there wil be " + aantalPogingen+" pogingen.");
+        if (aantalPogingen > prisioners){
+            System.out.println("ERROR: Het aantal pogingen is hoger dan het aantal dozen waar uit te kiezen is!");
+            System.exit(1);
+        }
+        System.out.println("The simulaties will be run...");
 
-
-
-	// write your code here
+        theSimulatie(prisioners, aantalPogingen);
     }
 
-    public void gameSetup(int prisioners){
+        // write your code here
+        public static boolean theSimulatie(int prisioners, int aantalPogingen){
+            HashMap bord = gameSetup(prisioners);
 
-    }
 
-    public ArrayList generrateListOfZeroToPrisioners(int prisioners){
-        ArrayList<Integer> zeroToPrisioners = new ArrayList<>();
-        int i = 1;
-        while (!(prisioners == 0)){
-            zeroToPrisioners.add(i);
-            i++;
-            prisioners--;
+
+            return true; //if won true if lost false
+        }
+
+
+        public static HashMap gameSetup ( int prisioners){
+            int boxes = 1;
+            int indexBoxes = 0;
+            ArrayList<Integer> cards = generrateListOfZeroToPrisioners(prisioners);
+            Collections.shuffle(cards);
+            HashMap<Integer,Integer> bord = new HashMap<>();
+            while (boxes <= prisioners) {
+
+
+                bord.put(boxes,cards.get(indexBoxes));
+                indexBoxes++;
+                boxes++;
+            }
+            return bord;
+
+
 
         }
-        return zeroToPrisioners;
-    }
+
+        public static ArrayList generrateListOfZeroToPrisioners ( int prisioners){
+            ArrayList<Integer> zeroToPrisioners = new ArrayList<>();
+            int i = 1;
+            while (!(prisioners == 0)) {
+                zeroToPrisioners.add(i);
+                i++;
+                prisioners--;
+
+            }
+            return zeroToPrisioners;
+        }
+
+
+
 }
